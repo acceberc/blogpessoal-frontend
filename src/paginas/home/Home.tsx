364c20f6e3fb91907354bbb4b59e32d96342d599
acceberc <rebecca-cunha@hotmail.com>
2022-03-21 11:53:27 -0300

    create project

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+        <h1 className="titulo">Home</h1>
+        <img src="https://www.ifrr.edu.br/midia/teste/image" alt="imagem tela inicial" className="img"></img>
+        </>
+    );
+}
+
+export default Home;

4a91d2796b06db4bfdf4661c5976dde64928a4de
acceberc <rebecca-cunha@hotmail.com>
2022-03-24 23:22:00 -0300

    adding login & refactoring css style

--- src/componentes/estaticos/footer/Footer.tsx
+++ src/componentes/estaticos/footer/Footer.tsx
@@ -1,37 +1,39 @@
 import React from 'react';
-import InstagramIcon from '@material-ui/icons/Instagram';
-import FacebookIcon from '@material-ui/icons/Facebook';
+import EmailIcon from '@mui/icons-material/Email';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 import {Typography, Box, Grid } from '@material-ui/core';
+
+import './Footer.css'
 
 function Footer(){
     return(
         <>
         <Grid container direction="row" justifyContent="center" alignItems="center">
-                <Grid alignItems="center" item xs={12}>
-                    <Box style={{ backgroundColor: "#3F51B5", height: "120px" }}>
+                <Grid alignItems="center" item xs={12} >
+                    <Box className="contatos">
                         <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Siga-nos nas redes sociais </Typography>
+                            <Typography variant="h5" align="center" gutterBottom className="textos">Contatos </Typography>
                         </Box>
                         <Box display="flex" alignItems="center" justifyContent="center">
-                            <a href="https://www.facebook.com/generationbrasil" target="_blank">
-                                <FacebookIcon style={{ fontSize: 60, color: "white" }} />
+                            <a href="mailto:rebecca-cunha@hotmail.com" target="_blank">
+                                <EmailIcon className="redes" />
                             </a>
-                            <a href="https://www.instagram.com/generationbrasil/" target="_blank">
-                                <InstagramIcon style={{ fontSize: 60, color: "white" }} />
+                            <a href="https://github.com/acceberc" target="_blank">
+                                <GitHubIcon className="redes" />
                             </a>
-                            <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank">
-                                <LinkedInIcon style={{ fontSize: 60, color: "white" }} />
+                            <a href="https://www.linkedin.com/in/acceberc/" target="_blank">
+                                <LinkedInIcon className="redes" />
                             </a>
                         </Box>
                     </Box>
-                    <Box style={{ backgroundColor: "#303F9F", height: "60px" }}>
+                    <Box className="rodape">
                         <Box paddingTop={1}>
-                            <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >© 2022 Copyright:</Typography>
+                            <Typography variant="subtitle2" align="center" gutterBottom className="textos" >© 2022 Copyright:</Typography>
                         </Box>
                         <Box>
                             <a target="_blank" href="https://brasil.generation.org">
-                                <Typography variant="subtitle2" gutterBottom style={{ color: "white" }} align="center">brasil.generation.org</Typography>
+                                <Typography variant="subtitle2" gutterBottom align="center" className="textos">brasil.generation.org</Typography>
                             </a>
                         </Box>
                     </Box>
--- src/componentes/estaticos/navbar/Navbar.tsx
+++ src/componentes/estaticos/navbar/Navbar.tsx
@@ -1,43 +1,48 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, Box } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+
+import './Navbar.css'
 
 function Navbar() {
     return (
         <>
-            <AppBar position="static">
+            <AppBar position="static" className="barra">
                 <Toolbar variant="dense">
-                    <Box style={{ cursor: "pointer" }} >
+                    <Box className='cursor'>
                         <Typography variant="h5" color="inherit">
-                            MeuBlog
+                            <iframe src="https://giphy.com/embed/2pM0CJ7vHXgXiaLgVJ" width="480" height="142" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
                         </Typography>
                     </Box>
 
                     <Box display="flex" justifyContent="start">
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Box mx={5} className='cursor'>
                             <Typography variant="h6" color="inherit">
                                 Home
                             </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Box mx={5} className='cursor'>
                             <Typography variant="h6" color="inherit">
                                 Postagens
                             </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Box mx={5} className='cursor'>
                             <Typography variant="h6" color="inherit">
                                 Temas
                             </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Box mx={5} className='cursor'>
                             <Typography variant="h6" color="inherit">
                                 Novos temas
                             </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
-                            <Typography variant="h6" color="inherit">
-                                Sair
-                            </Typography>
-                        </Box>
+                        <Link to='/login' className='text-decorator-none'>
+                            <Box mx={5} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    Sair
+                                </Typography>
+                            </Box>
+                        </Link>
                     </Box>
 
                 </Toolbar>
